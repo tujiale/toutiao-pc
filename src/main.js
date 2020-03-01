69b40fd25a1565198bfb05992009399d8156567e
tujiale <tujiale@outlook.com>
2020-03-01 23:50:56 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'// 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import '@/style/index.less'
 
 Vue.use(ElementUI) // 全局注册element-ui框架
 Vue.config.productionTip = false
