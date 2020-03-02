6627883e967a55e8e2f60adcea25776d3f0d3f7a
tujiale <tujiale@outlook.com>
2020-03-02 22:52:55 +0800

    登录页面绑定及校验

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'// 引入UI
+import axios from 'axios' // 引入axios
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import '@/style/index.less'
 
 Vue.use(ElementUI) // 全局注册element-ui框架
 Vue.config.productionTip = false
 
+// 配置axios的baseURL
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 配置公共的请求头地址
+// 将axios赋值给 Vue对象的原型属性
+Vue.prototype.$axios = axios
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,26 +8,26 @@
          <img src="../../assets/img/logo_index.png" alt="">
        </div>
        <!-- 表单 -->
-       <el-form style="margin-top:20px">
+       <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top:20px">
          <!-- 表单容器 -->
-         <el-form-item>
+         <el-form-item prop="mobile">
            <!-- 表单域 -->
-           <el-input placeholder="请输入手机号"></el-input>
+           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
          </el-form-item>
+         <el-form-item prop="code">
          <!-- 验证码 -->
-         <el-form-item>
-           <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+           <el-input v-model="loginForm.code" :model="loginForm"  :rules="loginRules" style="width:60%" placeholder="请输入验证码"></el-input>
            <!-- 放置一个按钮 -->
            <el-button style="float:right" plain>发送验证码</el-button>
          </el-form-item>
          <!-- 表单域 -->
-         <el-form-item>
-           <!-- 是否同意被人家坑 -->
-           <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+         <el-form-item prop="checked">
+           <!-- 是否同意条款 -->
+           <el-checkbox v-model="loginForm.checked">我已阅读同意用户协议和隐私条款</el-checkbox>
          </el-form-item>
          <!-- 按钮 -->
          <el-form-item>
-           <el-button style="width:100%" type="primary">登录</el-button>
+           <el-button @click="login" style="width:100%" type="primary">登录</el-button>
          </el-form-item>
        </el-form>
     </el-card>
@@ -36,8 +36,57 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否同意协议
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '您的手机号不能为空' },
+          { pattern: /^1[3-9]\d{9}$/, message: '您的手机号格式不正确' }],
+        code: [{ required: true, message: '您的验证码不能为空' },
+          { pattern: /^\d{6}$/, message: '验证码应该是6位数' }],
+        checked: [{
+          validator: function (rule, value, callback) {
+            value ? callback() : callback(new Error('您必须同意我们的条款'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // this.$refs.loginForm.validate(function (isOK) {
+      //   if (isOK) {
+      //     console.log('校验通过')
+      //   } else {
+      //     console.log('校验未通过')
+      //   }
+      // })
+      this.$refs.loginForm.validate().then(() => {
+        // 如果成功通过 校验就会到达 then
+        // alert(123)
+        this.$axios({
+          url: '/authorizations', // 请求地址
+          data: this.loginForm, // body请求体参数
+          method: 'post'
+        }).then(result => {
+          // 把钥匙放在兜里 也就是把token存于 本地缓存
+          window.localStorage.setItem('user-token', result.data.data.token)
+          this.$router.push('/') // 登录成功 跳转到home页
+        }).catch(() => {
+          this.$message({
+            message: '手机号或者验证码错误',
+            type: 'warning'
+          })
+        })
+      })
+    }
+  }
 }
+
 </script>
 
 <style lang='less' scoped>
